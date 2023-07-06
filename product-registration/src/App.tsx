f9bcce8e4f28b5eb2c538afed7b531b45d0bf7ac
Victoria <victoria-milan@outlook.com>
2023-07-06 14:41:42 -0300

    feat: creation of database request function

--- product-registration/src/App.tsx
+++ product-registration/src/App.tsx
@@ -1,9 +1,12 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import RegisterProductFormComponent from "./components/RegisterProductFormComponent";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
+      <ToastContainer></ToastContainer>
       <RegisterProductFormComponent />
     </>
   );
