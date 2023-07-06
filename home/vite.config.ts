a3cf8c8fb3be204141d87c693cfb45388ec817fd
Germana-1 <germanagerriane@yahoo.com.br>
2023-07-05 23:01:25 -0300

    feat: federation configuration

--- home/vite.config.ts
+++ home/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "home",
+      filename: "homePage.js",
+      exposes: {
+        "./HeaderComponent": "./src/components/HeaderComponent",
+      },
+      shared: ["react", "react-dom"],
+    }),
+    tsconfigPaths(),
+  ],
+
+  build: {
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
