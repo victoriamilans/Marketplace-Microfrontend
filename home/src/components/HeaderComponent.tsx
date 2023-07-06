11bbade658bf02007ad09ca8634fb4daccb4ffaf
Germana-1 <germanagerriane@yahoo.com.br>
2023-07-06 15:31:19 -0300

    feat: fixed position

--- home/src/components/HeaderComponent.tsx
+++ home/src/components/HeaderComponent.tsx
@@ -9,7 +9,7 @@ const HeaderComponent = () => {
   };
 
   return (
-    <nav className="bg-white shadow-lg">
+    <nav className="bg-white shadow-lg fixad fixed top-0 left-0 right-0">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-24 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
