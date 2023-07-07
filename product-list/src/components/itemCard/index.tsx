c984d8fcaa8d7b00c58082b914b528b07736bc17
Susimara Roberti <susimararoberti@outlook.com>
2023-07-07 11:14:04 -0300

    fix: corrigido estilização do item card

--- product-list/src/components/itemCard/index.tsx
+++ product-list/src/components/itemCard/index.tsx
@@ -29,8 +29,8 @@ const ItemCard: React.FC<Item> = (item) => {
   };
 
   return (
-    <article className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4 flex flex-wrap justify-around">
-      <div className="bg-white shadow-md shadow-gray-400 p-5 w-full">
+    <article className="sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4 flex flex-wrap justify-around">
+      <div className="bg-white shadow-md shadow-gray-400 p-5">
         <img src={item.image} alt="Imagem do produto" className="w-full mb-4" />
         <h3 className="text-lg font-semibold">{item.name}</h3>
         <p className="text-gray-500">{item.category}</p>
